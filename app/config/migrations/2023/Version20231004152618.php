23f046b2c87b6af9f509f16ebd784231e449e167
rconstant <constantromain30@gmail.com>
2023-10-04 17:39:05 +0200

    Entity and migration added

---
+++ app/config/migrations/2023/Version20231004152618.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231004152618 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create works table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE SEQUENCE works_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('
+            CREATE TABLE works (
+                id INT NOT NULL, 
+                internal_id VARCHAR(255) NOT NULL, 
+                contract_id INT DEFAULT NULL, 
+                name VARCHAR(255) NOT NULL, 
+                original_name VARCHAR(255) NOT NULL, 
+                synopsis TEXT DEFAULT NULL, 
+                year SMALLINT DEFAULT NULL, 
+                duration VARCHAR(255) DEFAULT NULL, 
+                nationality VARCHAR(255) DEFAULT NULL, 
+                created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+                created_by INT DEFAULT NULL, 
+                updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+                updated_by INT DEFAULT NULL, 
+                PRIMARY KEY(id)
+            )
+        ');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F6E50243BFDFB4D8 ON works (internal_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F6E502435E237E06 ON works (name)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F6E5024354561530 ON works (original_name)');
+        $this->addSql('CREATE INDEX IDX_F6E502432576E0FD ON works (contract_id)');
+        $this->addSql('CREATE INDEX IDX_F6E50243DE12AB56 ON works (created_by)');
+        $this->addSql('CREATE INDEX IDX_F6E5024316FE72E1 ON works (updated_by)');
+        $this->addSql('ALTER TABLE works ADD CONSTRAINT FK_F6E502432576E0FD FOREIGN KEY (contract_id) REFERENCES contracts (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE works ADD CONSTRAINT FK_F6E50243DE12AB56 FOREIGN KEY (created_by) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE works ADD CONSTRAINT FK_F6E5024316FE72E1 FOREIGN KEY (updated_by) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP SEQUENCE works_id_seq CASCADE');
+        $this->addSql('ALTER TABLE works DROP CONSTRAINT FK_F6E502432576E0FD');
+        $this->addSql('ALTER TABLE works DROP CONSTRAINT FK_F6E50243DE12AB56');
+        $this->addSql('ALTER TABLE works DROP CONSTRAINT FK_F6E5024316FE72E1');
+        $this->addSql('DROP TABLE works');
+    }
+}
---
+++ app/src/Entity/Work.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\Shared\BlameableEntity;
+use App\Entity\Shared\TimestampableEntity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'works')]
+class Work
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private int $id;
+
+    #[ORM\Column(unique: true)]
+    private string $internalId;
+
+    #[ORM\Column(unique: true)]
+    private string $name;
+
+    #[ORM\Column(unique: true)]
+    private string $originalName;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(type: Types::SMALLINT, length: 4, nullable: true)]
+    private ?int $year = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $duration = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $nationality = null;
+
+    #[ORM\ManyToOne(targetEntity: Contract::class, inversedBy: 'works')]
+    #[ORM\JoinColumn(name: 'contract_id', referencedColumnName: 'id')]
+    private Contract $contract;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getInternalId(): string
+    {
+        return $this->internalId;
+    }
+
+    public function setInternalId(string $internalId): static
+    {
+        $this->internalId = $internalId;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): static
+    {
+        $this->originalName = $originalName;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): static
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(?int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?string $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(?string $nationality): static
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+
+    public function getContract(): Contract
+    {
+        return $this->contract;
+    }
+
+    public function setContract(Contract $contract): static
+    {
+        $this->contract = $contract;
+
+        return $this;
+    }
+}
