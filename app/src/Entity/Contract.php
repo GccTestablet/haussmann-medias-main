fad53fdd1405e660862473ef53fda6e911e77c73
rconstant <constantromain30@gmail.com>
2023-10-04 10:57:15 +0200

    Beneficiary and Contract entity and migration added

---
+++ app/src/Entity/Contract.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\Shared\BlameableEntity;
+use App\Entity\Shared\TimestampableEntity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'contracts')]
+class Contract
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: Company::class)]
+    #[ORM\JoinColumn(name: 'company_id', referencedColumnName: 'id', nullable: true)]
+    private Company $company;
+
+    #[ORM\ManyToOne(targetEntity: Beneficiary::class)]
+    #[ORM\JoinColumn(name: 'beneficiary_id', referencedColumnName: 'id', nullable: true)]
+    private Beneficiary $beneficiary;
+
+    #[ORM\Column]
+    private string $fileName;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTime $signedAt;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTime $startsAt;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private \DateTime $endsAt;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $territories = null;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCompany(): Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(Company $company): static
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getBeneficiary(): Beneficiary
+    {
+        return $this->beneficiary;
+    }
+
+    public function setBeneficiary(Beneficiary $beneficiary): static
+    {
+        $this->beneficiary = $beneficiary;
+
+        return $this;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName(string $fileName): static
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    public function getSignedAt(): \DateTime
+    {
+        return $this->signedAt;
+    }
+
+    public function setSignedAt(\DateTime $signedAt): static
+    {
+        $this->signedAt = $signedAt;
+
+        return $this;
+    }
+
+    public function getStartsAt(): \DateTime
+    {
+        return $this->startsAt;
+    }
+
+    public function setStartsAt(\DateTime $startsAt): static
+    {
+        $this->startsAt = $startsAt;
+
+        return $this;
+    }
+
+    public function getEndsAt(): \DateTime
+    {
+        return $this->endsAt;
+    }
+
+    public function setEndsAt(\DateTime $endsAt): static
+    {
+        $this->endsAt = $endsAt;
+
+        return $this;
+    }
+
+    public function getTerritories(): ?string
+    {
+        return $this->territories;
+    }
+
+    public function setTerritories(?string $territories): static
+    {
+        $this->territories = $territories;
+
+        return $this;
+    }
+}
