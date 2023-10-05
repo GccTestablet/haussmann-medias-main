4c2b5e26b02d33f0d4b46f648897ba4e44c233a2
rconstant <constantromain30@gmail.com>
2023-10-05 13:21:18 +0200

    Feature: User company selector

--- app/src/Entity/Company.php
+++ app/src/Entity/Company.php
@@ -6,12 +6,13 @@ namespace App\Entity;
 
 use App\Entity\Shared\BlameableEntity;
 use App\Entity\Shared\TimestampableEntity;
+use App\Repository\CompanyRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: CompanyRepository::class)]
 #[ORM\Table(name: 'companies')]
 class Company
 {
