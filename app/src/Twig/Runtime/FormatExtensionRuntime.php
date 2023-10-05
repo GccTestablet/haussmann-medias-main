d38af1698032ffa1598db71376207f10447c1580
rconstant <constantromain30@gmail.com>
2023-10-05 10:03:59 +0200

    User format added

--- app/src/Twig/Runtime/FormatExtensionRuntime.php
+++ app/src/Twig/Runtime/FormatExtensionRuntime.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Twig\Runtime;
 
+use App\Entity\User;
 use App\Tools\Parser\DateParser;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class FormatExtensionRuntime implements RuntimeExtensionInterface
 {
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator
+    ) {}
+
     public function formatDate(?\DateTime $dateTime): ?string
     {
         return $dateTime?->format(DateParser::FR_DATE_FORMAT);
@@ -17,5 +23,18 @@ class FormatExtensionRuntime implements RuntimeExtensionInterface
     public function formatDateTime(?\DateTime $dateTime): ?string
     {
         return $dateTime?->format(DateParser::FR_DATETIME_FORMAT);
+    }
+
+    public function formatUser(?User $user): ?string
+    {
+        if (!$user) {
+            return null;
+        }
+
+        return \sprintf(
+            '<a href="%s">%s</a>',
+            $this->urlGenerator->generate('app_user_show', ['id' => $user->getId()]),
+            $user->getFullName()
+        );
     }
 }
