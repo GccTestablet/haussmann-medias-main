65e57242821964718dd28d4c602b50762f4bb2a8
Romain CONSTANT <40657908+rconstant@users.noreply.github.com>
2023-10-19 13:58:38 +0200

    Tests: Fixtures loader added (#47)

--- app/tests/AbstractTestCase.php
+++ app/tests/AbstractTestCase.php
@@ -4,21 +4,33 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Tests\Tools\Loader\DoctrineFixtureLoader;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 abstract class AbstractTestCase extends WebTestCase
 {
+    protected function getService(string $id): ?object
+    {
+        return self::getContainer()->get($id);
+    }
+
+    /**
+     * @param string[] $fixtures
+     */
+    protected function loadOrmOnDemandFixtures(array $fixtures, bool $append = false): void
+    {
+        /** @var DoctrineFixtureLoader $fixtureLoader */
+        $fixtureLoader = $this->getService(DoctrineFixtureLoader::class);
+
+        $fixtureLoader->loadFixtures($fixtures, $append);
+    }
+
     protected function getFile(string $path): string
     {
         /** @var KernelInterface $kernel */
         $kernel = $this->getService('kernel');
 
         return \sprintf('%s/%s', $kernel->getProjectDir(), $path);
-    }
-
-    protected function getService(string $id): ?object
-    {
-        return self::getContainer()->get($id);
     }
 }
---
+++ app/tests/Tools/Loader/DoctrineFixtureLoader.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Tools\Loader;
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class DoctrineFixtureLoader
+{
+    public function __construct(
+        private readonly ContainerInterface $container,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ORMExecutor $ORMExecutor,
+    ) {}
+
+    /**
+     * @param string[] $fixtureNames
+     */
+    public function loadFixtures(array $fixtureNames, bool $append = false): void
+    {
+        $loader = new ContainerAwareLoader($this->container);
+
+        foreach ($fixtureNames as $fixtureName) {
+            if (!\class_exists($fixtureName)) {
+                throw new NotFoundHttpException(\sprintf('Fixture "%s" not found', $fixtureName));
+            }
+
+            $loader->addFixture(new $fixtureName());
+        }
+
+        if (!$append) {
+            $this->truncateDatabase();
+        }
+
+        $this->ORMExecutor->execute($loader->getFixtures(), true);
+    }
+
+    private function truncateDatabase(): void
+    {
+        $connection = $this->entityManager->getConnection();
+
+        try {
+            $tables = $connection->createSchemaManager()->listTables();
+            $connection->beginTransaction();
+
+            foreach ($tables as $table) {
+                $connection->executeQuery(\sprintf('TRUNCATE %s CASCADE', $table->getName()));
+            }
+
+            $connection->commit();
+        } catch (Exception) {
+        }
+    }
+}
