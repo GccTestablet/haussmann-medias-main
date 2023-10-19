7776e6912d69d69fb81426dd52258ba16da8ea3e
rconstant <constantromain30@gmail.com>
2023-10-19 18:02:38 +0200

    Revert

---
+++ app/tests/Integration/Service/User/UserActivityManagerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Service\User;
+
+use App\Entity\User;
+use App\Service\User\UserActivityManager;
+use App\Tests\AbstractTestCase;
+use App\Tests\Fixtures\Doctrine\UserFixture;
+
+class UserActivityManagerTest extends AbstractTestCase
+{
+    private ?UserActivityManager $userActivityManager;
+
+    protected function setUp(): void
+    {
+        $this->loadOrmOnDemandFixtures([
+            UserFixture::class,
+        ]);
+
+        $this->userActivityManager = $this->getService(UserActivityManager::class);
+    }
+
+    public function testUpdateLastLogin(): void
+    {
+        /** @var User $user */
+        $user = $this->getReference(UserFixture::SUPER_ADMIN_USER);
+
+        $this->assertNull($user->getLastLogin());
+
+        $this->userActivityManager->updateLastLogin($user);
+
+        $this->assertInstanceOf(\DateTime::class, $user->getLastLogin());
+    }
+
+    public function testUpdateLastActivity(): void
+    {
+        /** @var User $user */
+        $user = $this->getReference(UserFixture::SUPER_ADMIN_USER);
+
+        $this->assertNull($user->getLastActivity());
+
+        $this->userActivityManager->updateLastActivity($user);
+
+        $this->assertInstanceOf(\DateTime::class, $user->getLastActivity());
+    }
+}
---
+++ app/tests/Integration/Service/User/UserManagerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Service\User;
+
+use App\Entity\User;
+use App\Service\User\UserManager;
+use App\Tests\AbstractTestCase;
+use App\Tests\Fixtures\Doctrine\UserFixture;
+
+class UserManagerTest extends AbstractTestCase
+{
+    private ?UserManager $userManager;
+
+    protected function setUp(): void
+    {
+        $this->loadOrmOnDemandFixtures([
+            UserFixture::class,
+        ]);
+
+        $this->userManager = $this->getService(UserManager::class);
+    }
+
+    public function testFindAll(): void
+    {
+        $this->assertCount(2, $this->userManager->findAll());
+    }
+
+    public function testFindByEmail(): void
+    {
+        $this->assertInstanceOf(User::class, $this->userManager->findByEmail('super-admin@hm.mail'));
+        $this->assertNull($this->userManager->findByEmail('no-existing@hm.mail'));
+    }
+
+    public function testFindByPasswordResetToken(): void
+    {
+        $this->assertInstanceOf(User::class, $this->userManager->findByPasswordResetToken('token'));
+        $this->assertNull($this->userManager->findByPasswordResetToken('no-existing-token'));
+    }
+}
