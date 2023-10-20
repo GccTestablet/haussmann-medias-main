0d0756a54ce5e729e8b4cf96c3959ba37ffc1f30
Romain CONSTANT <40657908+rconstant@users.noreply.github.com>
2023-10-20 13:55:58 +0200

    Design: Contract and work design updated (#53)

---
+++ app/assets/js/controllers/datatable/table_expand_controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller<HTMLTableElement> {
+  static targets = ["button", "row", "expandableRow"];
+
+  declare readonly buttonTarget: HTMLButtonElement;
+  declare readonly expandableRowTargets: HTMLTableRowElement[];
+
+  expand({ params: { work } }) {
+    this.expandableRowTargets.forEach((element) => {
+      const workId = element.getAttribute("data-work-id");
+      if (work !== parseInt(workId)) {
+        return;
+      }
+
+      element.classList.toggle("d-none");
+    });
+
+    this.buttonTarget.querySelector("i").classList.toggle("rotate-90");
+  }
+}
