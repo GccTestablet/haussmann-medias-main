08d02c42f77b56282de8581740b19ac33924dcde
GccTeam <testablet@gcc-med.com>
2023-10-23 17:31:23 +0200

    edit migration

--- app/config/migrations/2023/Version20231023132159.php
+++ app/config/migrations/2023/Version20231023132159.php
@@ -21,7 +21,6 @@ final class Version20231023132159 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE work_adaptations DROP comment');
     }
 }
