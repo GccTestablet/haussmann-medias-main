cee61944483078bdd92edb8ccb4be5d1e109eb9b
GccTeam <testablet@gcc-med.com>
2023-10-23 17:02:42 +0200

    edit migration

--- app/config/migrations/2023/Version20231023144456.php
+++ app/config/migrations/2023/Version20231023144456.php
@@ -21,8 +21,6 @@ final class Version20231023144456 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE works ADD currency NUMERIC(10, 2) DEFAULT NULL');
         $this->addSql('ALTER TABLE work_adaptations DROP comment');
     }
 }
