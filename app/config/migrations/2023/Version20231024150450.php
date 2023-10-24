56a1088095707b979be92efe8bdb5b27cdf9b1cb
GccTeam <testablet@gcc-med.com>
2023-10-24 22:20:55 +0200

    Edit Migrations

--- app/config/migrations/2023/Version20231024150450.php
+++ app/config/migrations/2023/Version20231024150450.php
@@ -18,7 +18,7 @@ final class Version20231024150450 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE works RENAME COLUMN minimum_guaranteed_before_reversion TO minimum_guaranteed');
-        $this->addSql('ALTER TABLE works RENAME COLUMN inimum_cost_of_the_top_before_reversion TO ceiling_of_recoverable_costs');
+        $this->addSql('ALTER TABLE works RENAME COLUMN minimum_cost_of_the_top_before_reversion TO ceiling_of_recoverable_costs');
     }
 
     public function down(Schema $schema): void
