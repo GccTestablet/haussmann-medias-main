785ad79664feddbd6d61a85d039db951742ea1d1
rconstant <constantromain30@gmail.com>
2023-11-02 10:53:44 +0100

    Cleanup

--- app/src/Form/Type/Work/WorkReversionFormType.php
+++ app/src/Form/Type/Work/WorkReversionFormType.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace App\Form\Type\Work;
 
-use App\Entity\Setting\BroadcastChannel;
 use App\Form\Dto\Work\WorkReversionFormDto;
-use App\Form\Type\Common\BroadcastChannelEntityField;
-use App\Repository\Broadcast\BroadcastChannelRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -28,25 +25,6 @@ class WorkReversionFormType extends AbstractType
                 ],
             ])
         ;
-        //        /** @var WorkReversionFormDto $dto */
-        //        $dto = $builder->getData();
-        //
-        //        $builder
-        //            ->add('channel', BroadcastChannelEntityField::class, [
-        // //                'placeholder' => 'Select a channel',
-        //                'required' => true,
-        // //                'class' => BroadcastChannel::class,
-        // //                'query_builder' => fn (BroadcastChannelRepository $repository) => $repository->getAvailableChannelByWorkQueryBuilder(
-        // //                    $dto->getWorkReversion()->getWork(),
-        // //                    $dto->getChannel()
-        // //                ),
-        // //                'choice_label' => 'name',
-        // //                'translation_domain' => 'setting',
-        //            ])
-        //            ->add('percentageReversion', NumberType::class, [
-        //                'required' => true,
-        //            ])
-        //        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
